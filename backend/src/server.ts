64b8f6fbd9a3eb773fd444a698bc153a099acae0
thefordz <amthefordz@gmail.com>
2026-06-07 15:04:27 +0700

    feat(backend): add cors for frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import exprese from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import registrationRouter from "../src/routes/registration.route";
 
@@ -7,6 +8,9 @@ const app = exprese();
 
 app.use(exprese.json());
 const PORT = process.env.PORT || 3000;
+
+const allowdOrigins = [process.env.FRONTEND_URL || "http://localhost:4000"];
+app.use(cors({ origin: allowdOrigins }));
 
 app.use("/api/registration", registrationRouter);
 
